529180e2215f51856c000eedb2431c53a77f2681
kenng8891 <103011617+kenng8891@users.noreply.github.com>
2022-08-21 16:34:05 -0400

    added server.js routes and date

---
+++ routes/thoughts-routes.js
@@ -0,0 +1,30 @@
+const router = require("express").Router;
+
+const { get } = require("http");
+const {
+  getThoughts,
+  getThoughtsbyId,
+  createThoughts,
+  updateThoughts,
+  deleteThoughts,
+  createReaction,
+  removeReaction,
+} = require("../controllers/thought-controllers");
+
+//create get post routes
+router.route("/").get(getThoughts).post(createThoughts);
+
+//CRUD routes using ID
+router
+  .route("/:id")
+  .get(getThoughtsbyId)
+  .put(updateThoughts)
+  .delete(deleteThoughts);
+
+//create reaction using thought ID
+router.route("/:thoughtId/reactions").post(createReaction);
+
+//delete reaction by thought ID
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
